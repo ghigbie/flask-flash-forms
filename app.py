3ac0fa6ef3c98afab9379e690701e4f440a4843d
George Higbie <ghigbie@monigle.com>
2019-03-13 09:44:19 -0600

    Add string interpolation

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 title = 'Flask Forms with Flash Messages'
 
 class SimpleForm(FlaskForm):
+    breed = StringField('What breed are you? ', validators=[DataRequired()])
     submit = SubmitField('Click Me')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SimpleForm()
     if form.validate_on_submit():
-        flash('You just clicked the button!')
+        session['breed'] = form.breed.data
+        flash(f'You just changed your breed to: {session["breed"]}')
         return redirect(url_for('index'))
     return render_template('index.html', form=form, title=title)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run(debug=True)
