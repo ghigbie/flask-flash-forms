9ee3326bd04e6b2680337a4c76e2677aaeb40cad
George Higbie <ghigbie@monigle.com>
2019-03-13 08:44:24 -0600

    Set up route and app run

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
+
+class SimpleForm(FlaskForm):
+    submit = SubmitField('Click Me')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SimpleForm()
+    if form.validate_on_submit():
+        flash('You just clicked the button!')
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
